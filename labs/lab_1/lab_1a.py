6467f04c107e3745fa06c987f0d9f748378b5ab9
Tanuj Ranjith <tanujranjith@gmail.com>
2026-02-22 13:21:08 -0500

    added my personal introduction from the start of class to lab 1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,8 @@ def main():
     name = "Tanuj Ranjith" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"Hi everyone, my name is Tanuj Ranjith. Personally I love robotics, coding, music, badminton, cooking, video games, movie watching, and going out with friends. For robotics I am part of VEX Robotics team 91C for the past 4 years and was with a different team for 2 years and before that I did FLL for 3 years, so i've been doing robotics my whole life and have built many robotics projects on the side. In my freetime i love to code projects and build apps to solve my personal issues. I also love to cook and see how different spices interact with each other to develop new flavors and reactions. Look forward to learning along side you guys!")
+
 
 if __name__ == "__main__":
     main()
